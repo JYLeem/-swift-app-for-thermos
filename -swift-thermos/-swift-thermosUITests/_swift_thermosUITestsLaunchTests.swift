ebf669884c1ed07920da70f941228f0cd588ad08
JYLeem <102199863+JYLeem@users.noreply.github.com>
2024-11-07 17:54:52 +0900

    Feat: Initial setting

---
+++ -swift-thermos/-swift-thermosUITests/_swift_thermosUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  _swift_thermosUITestsLaunchTests.swift
+//  -swift-thermosUITests
+//
+//  Created by Jinyoung Leem on 11/7/24.
+//
+
+import XCTest
+
+final class _swift_thermosUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
