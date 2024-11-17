1c2ac1e47b2996f92cc15a5707bd0daa2b1ab329
JYLeem <102199863+JYLeem@users.noreply.github.com>
2024-11-17 19:53:48 +0900

    Feat: Splash 화면 연결 및 3초 후 HomeViewController로 전환 구현, Bluetooth Info.list 추가

--- -swift-thermos/-swift-thermos/SceneDelegate.swift
+++ -swift-thermos/-swift-thermos/SceneDelegate.swift
@@ -10,14 +10,36 @@ import UIKit
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
+    
+//    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+//        guard let windowScene = (scene as? UIWindowScene) else { return }
+//        
+//        let window = UIWindow(windowScene: windowScene)
+//        window.overrideUserInterfaceStyle = UIUserInterfaceStyle.light
+//        
+//        let splashVC = ViewController()
+//        window.rootViewController = splashVC
+//        window.makeKeyAndVisible()
+//        
+//        self.window = window
+//        
+//        let rootVC = HomeViewController()
+//        
+//        DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
+//            let navigationController = UINavigationController(rootViewController: rootVC)
+//            
+//            self.window?.rootViewController = navigationController
+//        }
+//    }
 
 
-    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
-        // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
-        // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard let _ = (scene as? UIWindowScene) else { return }
-    }
+
+//    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+//        // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
+//        // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
+//        // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
+//        guard let _ = (scene as? UIWindowScene) else { return }
+//    }
 
     func sceneDidDisconnect(_ scene: UIScene) {
         // Called as the scene is being released by the system.
--- -swift-thermos/-swift-thermos/ViewController.swift
+++ -swift-thermos/-swift-thermos/ViewController.swift
@@ -9,6 +9,10 @@ class ViewController: UIViewController {
         setupSplashImage()
         setupGradientLayer()
         setupLabel()
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 3.0) {
+            self.transitionToHomeViewController()
+        }
     }
     
     // 이미지 설정 및 contentMode 적용
@@ -40,7 +44,7 @@ class ViewController: UIViewController {
         let label = UILabel()
         label.text = "\(RootSetting.businessName)"
         label.textColor = .white
-        label.font = UIFont(name: "Lancelot", size: 50)
+        label.font = UIFont.boldSystemFont(ofSize: 30)
         label.textAlignment = .center
         label.translatesAutoresizingMaskIntoConstraints = false
         
@@ -51,5 +55,20 @@ class ViewController: UIViewController {
             label.centerXAnchor.constraint(equalTo: splashImage.centerXAnchor),
             label.centerYAnchor.constraint(equalTo: splashImage.centerYAnchor)
         ])
+    }
+    
+    // HomeViewController로 전환
+    private func transitionToHomeViewController() {
+        // let homeViewController = HomeViewController()
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let homeViewController = storyboard.instantiateViewController(withIdentifier: "HomeVC") as! HomeViewController
+        let navigationController = UINavigationController(rootViewController: homeViewController)
+        
+        // 윈도우의 rootViewController를 변경
+        if let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene,
+           let window = windowScene.windows.first {
+            window.rootViewController = navigationController
+            UIView.transition(with: window, duration: 0.5, options: .transitionCrossDissolve, animations: nil)
+        }
     }
 }
